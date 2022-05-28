91919c15af53386c89180a54ea290fab66f86fd9
Andrew <84029016+AndrewSae@users.noreply.github.com>
2022-05-27 20:32:21 -0400

    simplified code

--- Led Blink/main.py
+++ Led Blink/main.py
@@ -3,8 +3,6 @@ from time import sleep
 
 led = Pin(15,Pin.OUT) 
 
-while True: # a infinate loop
-	led.value(1) # turns on the led    
-	sleep(0.5) # pauses the code for 0.5 seconds 
-	led.value(0) # turns off the led 
-	sleep(0.5)
+while True: 
+	led.toggle()    
+	sleep(0.5) 
