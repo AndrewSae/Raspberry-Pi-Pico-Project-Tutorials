3326ea001e1045eaeb96bab09904286676277962
AndrewSae <Andrewcsaenger@outlook.com>
2022-05-29 17:44:45 -0400

    added code for project

---
+++ OLED Display/main.py
@@ -0,0 +1,16 @@
+bfrom machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+width = 128
+height = 64
+
+i2c = I2C(0, scl = Pin(17), sda = Pin(16))
+oled = SSD1306_I2C(width, height, i2c)
+
+oled.fill(0)
+oled.text("hello",0,0)
+oled.text("from pi",0,10)
+oled.text("pico",0,20)
+oled.show()
+
+
