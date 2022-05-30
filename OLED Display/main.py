98ebf934923764079c8de34bed8b055b2eebe74c
Andrew <84029016+AndrewSae@users.noreply.github.com>
2022-05-30 16:54:23 -0400

    Changed code

--- OLED Display/main.py
+++ OLED Display/main.py
@@ -8,9 +8,5 @@ i2c = I2C(0, scl = Pin(17), sda = Pin(16))
 oled = SSD1306_I2C(width, height, i2c)
 
 oled.fill(0)
-oled.text("hello",0,0)
-oled.text("from pi",0,10)
-oled.text("pico",0,20)
+oled.text("Hello World!", 0,0)
 oled.show()
-
-
